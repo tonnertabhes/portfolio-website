c687cfe135cae53033941505aad54c1f1c39b13f
tonnertabhes <frontend@ambrosia.money>
2023-07-01 04:01:12 -0400

    Changing fetch address for resume download

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -57,7 +57,7 @@ export default function Navbar({
       redirect: "follow",
     };
 
-    fetch("https://localhost:8080/download", requestOptions)
+    fetch("https://williamkristiansen.art/download", requestOptions)
       .then((response) => response.blob())
       .then((result) => {
         const href = window.URL.createObjectURL(result);
